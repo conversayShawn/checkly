51645d904210053b01755b361296f9fd375d512b
conversayShawn <conversayshawn@gmail.com>
2026-01-15 17:28:14 -0500

    updated file name to reflect verbiage in email

--- checkly.config.js
+++ checkly.config.js
@@ -6,16 +6,25 @@ export default defineConfig({
   logicalId: 'otel-take-home',
   checks: {
     locations: ['af-south-1'],
-    browserChecks: {
-      testMatch: "__checks__/*.spec.js"
-    },
+    // browserChecks: {
+    //   testMatch: "__checks__/*.spec.js", 
+    // },
     playwrightConfigPath: "playwright.config.js",
-    playwrightChecks: [{
-      name: 'Firefox10',
-      logicalId: 'firefox-10',
-      pwProjects: ['firefox'],
-      tags: ['Firefox'],
-      frequency: Frequency.EVERY_5M
-    }]
+    playwrightChecks: [
+      {
+        name: 'Firefox10',
+        logicalId: 'firefox-10',
+        pwProjects: ['firefox'],
+        tags: ['Firefox'],
+        frequency: Frequency.EVERY_5M,
+        testMatch: "__checks__/playwright.spec.js"
+      },
+      {
+        name: 'Flaky',
+        logicalId: 'degraded-failed',
+        tags: ['UI'],
+        testMatch: "__checks__/browser.spec.js"
+      }
+    ]
   },
 })
