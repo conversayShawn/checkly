c20558351ffb1b060f28637aec7cbc8c0b2adeb4
conversayShawn <conversayshawn@gmail.com>
2026-01-15 15:43:09 -0500

    10 ff tests

--- checkly.config.js
+++ checkly.config.js
@@ -1,13 +1,12 @@
-// const {defineConfig} = require('@checkly/cli')
 import { defineConfig } from 'checkly' 
-// import { CheckGroupV2 } from 'checkly/constructs'
-
-// const group = new CheckGroupV2('ChecklyChallenge', {
-//   name: 'Checkly Challenge',
-//   activated: true
-// })
 
 export default defineConfig({
-  projectName: 'Website Monitoring',
-  logicalId: 'website-monitoring-1',
+  projectName: 'OTel Take Home',
+  logicalId: 'otel-take-home',
+  checks: {
+    locations: ['af-south-1'],
+    browserChecks: {
+      testMatch: "__checks__/*.spec.js"
+    },
+  }
 })
--- playwright.config.js
+++ playwright.config.js
@@ -1,39 +1,12 @@
-const { defineConfig, devices } = require('@playwright/test')
+import { defineConfig, devices } from '@playwright/test'
 
-module.exports = defineConfig({
-  timeout: 30000,
-    // Look for test files in the 'tests' directory, relative to this configuration file.
-  testDir: './tests',
-
-  // Run all tests in parallel.
-  fullyParallel: true,
-
-  // Fail the build on CI if you accidentally left test.only in the source code.
-  forbidOnly: !!process.env.CI,
-
-  // Retry tests on CI only.
-  retries: process.env.CI ? 2 : 0,
-
-  // Opt out of parallel tests on CI.
-  workers: process.env.CI ? 1 : undefined,
-
-  // Reporter to use
-  reporter: 'html',
-
+export default defineConfig({
+  testDir: '__checks__/*.spec.js',
   use: {
-    // Base URL to use in actions like `await page.goto('/')`.
-    baseURL: 'https://www.danube-web.shop',
-    viewport: { width: 1280, height: 720 },
-    // Always collect trace
-    trace: 'on',
+    baseURL: 'http://3.143.230.92:8080',
   },
   
-  // Configure projects for major browsers.
   projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
