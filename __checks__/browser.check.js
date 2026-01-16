96ad2ec394c9f5b3b21d647f04079dae23b35ff6
conversayShawn <conversayshawn@gmail.com>
2026-01-15 21:22:16 -0500

    add tags

--- __checks__/browser.check.js
+++ __checks__/browser.check.js
@@ -3,6 +3,7 @@ import { BrowserCheck, Frequency } from "checkly/constructs";
 new BrowserCheck('intermittent-degrade_fail-browser-check', {
     name: 'intermittent critical browser check',
     Frequency: Frequency.EVERY_1M,
+    tags: ['UI'],
     code:
     {
         entrypoint: new URL('browser.spec.js', import.meta.url).pathname
