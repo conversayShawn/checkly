fb867f8d5f27912e691899ea818fb811ec23bef6
conversayShawn <conversayshawn@gmail.com>
2026-01-16 23:59:59 -0500

    generate dynamic session id

--- src/specs/multistep.spec.js
+++ src/specs/multistep.spec.js
@@ -1,8 +1,8 @@
 import { test, expect } from "@playwright/test"
 
 test('multistep', async ({request}) => {
-    const sessionId = '965c154c-d09c-4252-87c9-01fe67e58527'
-    
+    const sessionId = `session-${Math.random().toString(36).substring(2, 9)}`
+
     await test.step('POST', async () => {
         
         const postResponse = await request.post(`${process.env.AWS_URL}/api/cart`, {
@@ -11,11 +11,11 @@ test('multistep', async ({request}) => {
                 item: { productId: "0PUK6V6EV0", quantity: 1 }
             }
         })
-        expect(postResponse.ok()).toBeOK
+        expect(postResponse.status()).toBe(200)
     })
     await test.step('GET', async () => {
         const getResponse = await request.get(`${process.env.AWS_URL}/api/cart?sessionId=${sessionId}`)
-        expect(getResponse.ok()).toBeOK
+        expect(getResponse.status()).toBe(200)
 
         const cartData = await getResponse.json()
         // console.log(JSON.stringify(cartData, null, 1));
