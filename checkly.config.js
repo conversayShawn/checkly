296f1f014c259c02b83d9f95fc59a525d30f77e4
conversayShawn <conversayshawn@gmail.com>
2026-01-19 23:09:13 -0500

    firefox battle

--- checkly.config.js
+++ checkly.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   logicalId: 'otel-take-home',
   repoUrl: 'https://github.com/conversayShawn/checkly',
   checks: {
-    playwrightConfigPath: 'playwright.config.js',
+    // playwrightConfigPath: 'playwright.config.js',
     runtimeId: '2025.04',
   },
 })
